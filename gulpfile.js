439914fe0484e24d5f969b355f4c67c49467b9b4
Jonathan Shoaf <you@example.com>
2015-08-11 16:40:36 -0400

    added compass process

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
   gutil = require('gulp-util'),
   coffee = require('gulp-coffee'),
   browserify = require('gulp-browserify'),
+  compass = require('gulp-compass'),
   concat = require('gulp-concat');
 
 // optional
@@ -12,6 +13,7 @@ var jsSources = [
   'components/scripts/tagline.js',
   'components/scripts/template.js'
 ];
+var sassSources = ['components/sass/style.css'];
 
 gulp.task('log', function() {
   gutil.log('workflows are awesome');
@@ -29,4 +31,15 @@ gulp.task('js', function() {
     .pipe(concat('script.js'))
     .pipe(browserify())
     .pipe(gulp.dest('builds/development/js'));
+});
+
+gulp.task('compass', function() {
+  gulp.src(sassSources)
+    .pipe(compass({
+      sass: 'components/sass',
+      image: 'builds/development/images',
+      style: 'expanded'
+    }) // or use a config.rb file (ruby)
+      .on('error', gutil.log))
+    .pipe(gulp.dest('builds/development/css'));
 });
