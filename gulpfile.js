554cfec9dec03096ef784b49d7079641cd948ceb
Jonathan Shoaf <you@example.com>
2015-08-12 10:52:19 -0400

    added env vars

--- gulpfile.js
+++ gulpfile.js
@@ -6,17 +6,41 @@ var gulp = require('gulp'),
   connect = require('gulp-connect'),
   concat = require('gulp-concat');
 
-// optional
-var coffeeSources = ['components/coffee/tagline.coffee'];
-var jsSources = [
+var env,
+  coffeeSources,
+  jsSources,
+  sassSources,
+  htmlSources,
+  jsonSources,
+  outputDir,
+  sassStyle;
+
+// To use, set the variable from the command line as shown:
+//   set NODE_ENV=production
+env = process.env.NODE_ENV || 'development';
+
+if (env === 'development') {
+  outputDir = 'builds/development/'
+  sassStyle = 'expanded';
+} else {
+  outputDir = 'builds/production/'
+  sassStyle = 'compressed';
+}
+
+console.log("[ENVIRONMENT MODE]: " + env);
+console.log("[outputDir]: " + outputDir);
+console.log("[sassStyle]: " + sassStyle);
+
+coffeeSources = ['components/coffee/tagline.coffee'];
+jsSources = [
   'components/scripts/rclick.js',
   'components/scripts/pixgrid.js',
   'components/scripts/tagline.js',
   'components/scripts/template.js'
 ];
-var sassSources = ['components/sass/style.scss'];
-var htmlSources = ['builds/development/*.html'];
-var jsonSources = ['builds/development/js/*.json'];
+sassSources = ['components/sass/style.scss'];
+htmlSources = [outputDir + '*.html'];
+jsonSources = [outputDir + 'js/*.json'];
 
 gulp.task('log', function() {
   gutil.log('workflows are awesome');
@@ -33,7 +57,7 @@ gulp.task('js', function() {
   gulp.src(jsSources)
     .pipe(concat('script.js'))
     .pipe(browserify())
-    .pipe(gulp.dest('builds/development/js'))
+    .pipe(gulp.dest(outputDir + 'js'))
     .pipe(connect.reload());
 });
 
@@ -41,9 +65,9 @@ gulp.task('compass', function() {
   gulp.src(sassSources)
     .pipe(compass({
       sass: 'components/sass',
-      css: 'builds/development/css',
-      image: 'builds/development/images',
-      style: 'expanded'
+      css: outputDir + 'css',
+      image: outputDir + 'images',
+      style: sassStyle
     }) // or use a config.rb file (ruby)
       .on('error', gutil.log))
     .pipe(connect.reload());
@@ -52,7 +76,7 @@ gulp.task('compass', function() {
 
 gulp.task('connect', function () {
   connect.server({
-    root: 'builds/development/',
+    root: outputDir,
     port: 9000,
     livereload: true
   });
